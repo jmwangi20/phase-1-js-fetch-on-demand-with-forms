e2299d07cad6f73e4faae01d39166300f0222819
John Kihugo Mwangi <johnmwangi13119@gmail.com>
2025-01-28 17:30:35 +0300

    fetch on demand with forms

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 const init = () => {
+    const inputForm = document.querySelector("form")
+
+    inputForm.addEventListener("submit",event => {
+
+        event.preventDefault()
+        const input = event.target["searchByID"].value
+        inputForm.reset()
+
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then(res => res.json())
+        .then(data => {
+            const title = document.querySelector("section#movieDetails h4")
+            const summary = document.querySelector("section#movieDetails p")
+
+
+        // change the contents of the summary and title base on the user input 
+            title.textContent = data.title;
+            summary.textContent = data.summary;
+        })
+    })
   
 }
 
